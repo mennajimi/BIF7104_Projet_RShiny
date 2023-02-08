94c253840a80ffa96740ff82604627966f9f36a8
mennajimi <99920006+mennajimi@users.noreply.github.com>
2023-02-08 11:57:27 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# BIF7104_Projet_RShiny
-Développement d’une application R shiny pour la visualisation de données RNA-seq 
+# Projet-Rshiny
+
+<h4> Setup pour git: </h4> 
+1) Créer un compte github
+2) Setup une clé ssh :
+	- En ligne de commande sur l'ordinateur personnel taper la commande: ssh-keygen -t rsa
+	- Se connecter sur son compte github, aller dans settings, puis dans SSH and GPG key. 
+	  Ajouter un nouvelle cle, puis copier coller le contenu du fichier id_rsa.pub
+3) Créer un repertoire local:
+	1) En ligne de commande, sur son desktop taper la commande: git clone git@github.com:sofiaff/Projet-Rshiny.git
+
+<br/>	
+<h4> Paquets à installer pour que l'application fonctionne:</h4> <br/>
+	install.packages("shiny")<br/>
+	install.packages("ggplot2")<br/>
+	install.packages("gprofiler2")<br/>
+	install.packages("ggrepel")<br/>
+<br/>	
+<h4> Rouler l'application: </h4>  <br/> 
+	Ouvrir Rstudio<br/>
+	Ouvrir le fichier app.R<br/>
+	Dans jobs rouler app.R<br/>
+<br/>
+<h4> Format du fichier à chargé: </h4>  <br/> 
+	Fichier au format CSV<br/> 
+	Filtrer le fichier en fonction des p-value du plus petit au plus grand<br/>
+	
+<h4> Commandes git: </h4>  <br/>
+*	Copier le repertoire : git clone <br/>
+*	Vérifier le status du projet: git status <br/>
+*	Montrer les changements: git diff <br/>
+*	Mettre à jour le répertoire local: git pull  (DOIT ETRE FAIT À CHAQUE FOIS AVANT DE COMMENCER À TRAVAILLER)<br/>
+*	Ajouter les modifications localement : git add <fichiers> <br/>
+*	Commit et ajout de commentaire: git commit <br/>
+*	Soumettre les changements au répertoire distant: git push <br/>
+*	Changer ou créer une branche: git checkout -b NomDeLaBranche <br/>
+*	Voir l'historique des changement: git log <br/>
+
+	
+Pour Mac: Si erreur lors de l'affichage du Heatmap installer le paquet suivant:<br/>
+	installer XQuartz:https://www.xquartz.org
---
+++ app.R
@@ -0,0 +1,163 @@
+options(shiny.maxRequestSize = 10 * 1024^2)
+library("ggplot2") 
+library("ggfortify")
+library("gprofiler2")
+library("ggrepel")
+shinyApp(
+  ui = tagList(
+    navbarPage("Projet BIF7104",
+      tabPanel("Data Table",
+               h4("Table"),
+               dataTableOutput("table"),
+               sidebarPanel(
+                 fileInput("file1", "Choose CSV File", accept = ".csv"),
+                 checkboxInput("header", "Header", TRUE),
+               )
+      ),
+      tabPanel("Volcano Plot",
+               h4("Volcano Plot"),
+               plotOutput("volcano_plot",
+                          width = "70%",
+                          height = "600px",
+                          hover = "volcano_hover",
+                          click = "volcano_click",
+                          dblclick = "volcano_dbl_click",
+                          brush = brushOpts(
+                            id = "volcano_brush",
+                            resetOnNew = TRUE)),
+               sidebarPanel(
+                 sliderInput("p_value_axis",
+                             "Set pvalue axis",
+                             min = 0,
+                             max = 100,
+                             value = 50),
+                 sliderInput("h_treshold",
+                             "Set pvalue treshold",
+                             min = 0,
+                             max = 50,
+                             value = 10),
+                 textInput("titleVolcano","Diagram title:",value=""),
+               )
+      ),
+      
+      tabPanel("PCA", 
+               h4("PCA plot"),
+               plotOutput("PCAPlot"),
+               textInput("titre","Diagram title:",value="")
+      ),
+      tabPanel("Heatmap",
+             h4("Heatmap"),
+             plotOutput("HeatmapPlot",
+                        width = "80%",
+                        height = "600px",),
+             sliderInput("p_value",
+                         "Set pvalue target",
+                         min = 0,
+                         max = 0.05,
+                         value = 0.005,
+                         step = 0.0001),
+             sliderInput("numberGene",
+                         "Set max number of gene:",
+                         min = 0,
+                         max = 1000,
+                         value = 500),
+             textInput("titreHeatmap","Diagram title:",value=""),
+      ),
+      tabPanel("Gene Ontology",
+             h4("Gene Ontology"),
+             uiOutput('GenePlot'),
+             selectInput("specie", "Species", c("hsapiens","mmusculus")),
+             uiOutput("var_ui")
+      ),
+      tabPanel("Gene Ontology Table",
+               h4("Gene Ontology Table"),
+               plotOutput('GeneTable',
+                          width = "100%",),
+      ),
+    )
+  ),
+  server = function(input, output) {
+    
+    myfile<- reactive({
+      mydata<-input$file1
+      cleanfile<-na.omit(read.csv(mydata$datapath, header = input$header))
+    })
+    
+    output$table <- renderDataTable({
+      myfile()
+    })
+    output$volcano_plot <- renderPlot({
+      ggplot(data=myfile(), aes(x=log2FoldChange, y=-log10(padj), colour = -log10(padj)>input$h_treshold), label=external_gene_name) + 
+        geom_point() + 
+        ggtitle(input$titleVolcano) +
+        theme_minimal()+
+        theme(plot.title = element_text(hjust = 0.5))+
+        ylim(0,input$p_value_axis)+
+        scale_color_manual(values=c("Blue", "red")) +
+        geom_hline(yintercept=input$h_treshold, col="red")+
+        geom_text(aes(label=ifelse(-log10(padj)>input$h_treshold,as.character(external_gene_name),'')),hjust=0,vjust=0)
+      })
+    output$PCAPlot <- renderPlot({
+      file<- input$file1
+      data<-na.omit(read.csv(file$datapath, header = input$header))
+      data <- data.frame(data)
+      data_cts <- na.omit(data[,(5:10)])
+      datafinale <- t(data_cts)
+      z <- datafinale[ , which(apply(datafinale, 2, var) != 0)]
+      pcaB<-prcomp(z, center = TRUE, scale. = TRUE)
+      pcaB.var<-pcaB$sdev^2
+      pcaB.var.per <- round(pcaB.var/sum(pcaB.var)*100, 1)
+      pcaB.data <- data.frame(Sample= rownames(pcaB$x), X=pcaB$x[,1], Y=pcaB$x[,2])
+      ggplot(data= pcaB.data, mapping= aes(x=X, y=Y, label=Sample), shape= sample)+
+        geom_point(shape=18, color= "#00AFBB")+
+        geom_text_repel( max.overlaps = getOption("ggrepel.max.overlaps", default = 20))+
+        xlab(paste("PC1 - ", pcaB.var.per[1],"%", sep="")) + 
+        ylab(paste("PC2 - ", pcaB.var.per[2],"%", sep="")) +
+        theme_bw() +
+        ggtitle(input$titre) +
+        theme(plot.title = element_text(hjust = 0.5))
+    })
+    output$HeatmapPlot <- renderPlot({
+      file<- input$file1
+      data<-na.omit(read.csv(file$datapath, header = input$header))
+      vars<- c("external_gene_name","n1_d1", "n2_d1","n3_d1","n1_d2","n2_d2", "n3_d2","padj")
+      datafinal<-data[vars]
+      rownames(datafinal)<-make.names(datafinal$external_gene_name, unique=TRUE)
+      datafinal1<-head(datafinal,input$numberGene)
+      datafinal2<-subset(datafinal,datafinal$padj<input$p_value)
+      if (nrow(datafinal1)<=nrow(datafinal2)) {
+        datafinal<-datafinal1
+      } else {
+        datafinal<-datafinal2
+      }
+      datafinal<-data.matrix(datafinal[,2:7])
+      heatmap(datafinal, scale="row", main=input$titreHeatmap)
+      
+    })
+    output$var_ui <- renderUI({
+      vars<- c("external_gene_name","ensembl_gene_id")
+      datafinal<-myfile()[vars]
+      databis<-datafinal[,1]
+      selectInput("var", "choose variable:", choices=databis, multiple=TRUE)
+    })
+    output$GenePlot <- renderUI({
+      gostres <- gost(query = list(c(input$var),
+                                   organism = input$specie))
+      names(gostres)
+      gostres
+      gostplot(gostres, capped = TRUE, interactive = TRUE)
+    })
+    output$GeneTable <- renderPlot({
+      gostres <- gost(query = list(c(input$var),
+                                   organism = input$specie))
+      publish_gosttable(
+        gostres,
+        highlight_terms = NULL,
+        use_colors = TRUE,
+        show_columns = c("source", "term_name", "term_size", "intersection_size"),
+        filename = NULL,
+        ggplot = TRUE
+      )
+    })
+  }
+)
